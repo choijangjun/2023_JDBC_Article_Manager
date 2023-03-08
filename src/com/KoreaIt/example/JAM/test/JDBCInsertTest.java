624992b944d5151ca679dad0c21acf01a97b40fc
choijangjun <jangjun9644@gmail.com>
2023-03-08 15:12:58 +0900

    article list시 DB에 있는 데이터 리스팅

---
+++ src/com/KoreaIt/example/JAM/test/JDBCInsertTest.java
@@ -0,0 +1,14 @@
+package com.KoreaIt.example.JAM.test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class JDBCInsertTest {
+	
+
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ src/com/KoreaIt/example/JAM/test/JDBCSelectTest.java
@@ -0,0 +1,17 @@
+package com.KoreaIt.example.JAM.test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.KoreaIt.example.JAM.Article;
+
+public class JDBCSelectTest {
+	public static void main(String[] args) {
+		
+	}
+}
